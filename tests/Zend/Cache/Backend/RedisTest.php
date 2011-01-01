753bdc919b73e8f320b851f1bb7d284f0d99aae0
Rostislav Mykhajliw <necromant2005@gmail.com>
2011-01-01 18:42:34 +0200

    init env

--- tests/Zend/Cache/Backend/RedisTest.php
+++ tests/Zend/Cache/Backend/RedisTest.php
@@ -2,8 +2,13 @@
 
 namespace Test;
 
+use Zend\Cache\Backend as CacheBackend;
+
 class RedisTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testInit()
+    {
+        $backend = new CacheBackend\Redis();
+    }
 }
 
